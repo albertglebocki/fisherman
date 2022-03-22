62f14f475f3062ed36e8f936fcea68e1e452f656
Dox <doxofficial1235@gmail.com>
2022-03-22 16:33:08 +0100

    Color Update
    Changed text colors to better match the whole "Fisherman" concept.

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ const Reset = '\x1b[0m'
 const Bold = '\x1b[1m'
 const Dim = '\x1b[2m'
 const Underline = '\x1b[4m'
-const FgGreen = '\x1b[32m'
-const FgMagenta = '\x1b[35m'
-const FgCyan = '\x1b[36m'
+const Green = '\x1b[32m'
+const Yellow = '\x1b[33m'
 
 // Main Server Function
 function startServer() {
@@ -39,10 +38,10 @@ function startServer() {
         var geo = geoip.lookup(ip);
 
         // Server Side Responses (Admin Feedback)
-        console.log(Bold + FgCyan + '\nNew Request Received ✔️\n' + Reset)
-        console.log(FgGreen + 'IP: ' + Reset, Underline + ip + Reset)
-        console.log(FgGreen + 'User-Agent: ' + Reset, userAgent)
-        console.log(FgGreen + 'Geo: ' + Reset, JSON.stringify(geo, null, 2) + '\n')
+        console.log(Bold + Green + '\nNew Request Received ✓\n' + Reset)
+        console.log(Yellow + 'IP: ' + Reset, Underline + ip + Reset)
+        console.log(Yellow + 'User-Agent: ' + Reset, userAgent)
+        console.log(Yellow + 'Geo: ' + Reset, JSON.stringify(geo, null, 2) + '\n')
 
         //Client Side Responses (Client View)
         res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -50,7 +49,7 @@ function startServer() {
 
     // App Listener
     app.listen(port, () => {
-        console.log(FgMagenta + ' _____ _     _                                     \n|  ___(_)___| |__   ___ _ __ _ __ ___   __ _ _ __  \n| |_  | / __| \'_ \\ / _ \\ \'__| \'_ \` _ \\ / _` | \'_ \\ \n|  _| | \\__ \\ | | |  __/ |  | | | | | | (_| | | | |\n|_|   |_|___/_| |_|\\___|_|  |_| |_| |_|\\__,_|_| |_|')
+        console.log(Yellow + ' _____ _     _                                     \n|  ___(_)___| |__   ___ _ __ _ __ ___   __ _ _ __  \n| |_  | / __| \'_ \\ / _ \\ \'__| \'_ \` _ \\ / _` | \'_ \\ \n|  _| | \\__ \\ | | |  __/ |  | | | | | | (_| | | | |\n|_|   |_|___/_| |_|\\___|_|  |_| |_| |_|\\__,_|_| |_|')
         console.log(Dim + 'By Dox-Dev (https://github.com/dox-dev)\n' + Reset)
     })
 }

6fa257911c5c625612f2b2fc60af9704db02c3d5
Dox <doxofficial1235@gmail.com>
2022-03-22 18:10:45 +0100

    Update Fixing Bug
    There was a small bug in "app.js". We were using color, that didn't exist anymore :100:

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function startServer() {
     // Creating Ngrok Tunnel
     (async function() {
         const url = await ngrok.connect({authtoken: token, addr: 8080})
-        console.log(Bold + FgMagenta + 'Your url: ' + Reset, url)
+        console.log(Bold + Yellow + 'Your url: ' + Reset, url)
     })();
 
     // App Request Handler
